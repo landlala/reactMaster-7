dca3f35f88b9b1786107ae477f44261119d4fb4f
landlala <land_lala@naver.com>
2023-04-01 02:02:42 +0000

    Initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,43 @@
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+
+function App() {
+  const onDragEnd = ()=> {
+    
+  };
+  return (
+    <DragDropContext onDragEnd = {onDragEnd}>
+      <div>
+        <Droppable droppableId = "one">
+          {(magic) => (
+            <ul ref = {magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId = "first" index = {0}>
+                {(magic) => (
+                  <li 
+                    ref = {magic.innerRef} 
+                    {...magic.draggableProps} 
+                  > 
+                    <span {...magic.dragHandleProps}>💖</span>
+                    one
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId = "second" index = {1}>
+                {(magic) => (
+                  <li 
+                    ref = {magic.innerRef} 
+                    {...magic.draggableProps} 
+                  >
+                    <span {...magic.dragHandleProps}>💖</span>
+                    two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+}
+
+export default App;
