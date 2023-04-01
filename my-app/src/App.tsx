b89f826550b7315955c13857e4aba75a21a85905
landlala <land_lala@naver.com>
2023-04-01 04:37:36 +0000

    #7.6

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -36,8 +36,14 @@ const Card = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
-  const onDragEnd = ({destination, source}: DropResult)=> {
-    
+  const onDragEnd = ({draggableId, destination, source}: DropResult)=> {
+    if(!destination) return;
+    setToDos(oldToDos => {
+      const toDosCopy = [...oldToDos];
+      toDosCopy.splice(source.index, 1)
+      toDosCopy.splice(destination?.index, 0, draggableId)
+      return toDosCopy; 
+    });
   };
   return (
     <DragDropContext onDragEnd = {onDragEnd}>
@@ -45,7 +51,7 @@ function App() {
         <Droppable droppableId = "one">
           {(magic) => (
             <Board ref = {magic.innerRef} {...magic.droppableProps}>
-              {toDos.map((toDo, index) => <Draggable key = {index} draggableId = {toDo} index = {index}>
+              {toDos.map((toDo, index) => <Draggable key = {toDo} draggableId = {toDo} index = {index}>
                 {(magic) => (
                   <Card 
                     ref = {magic.innerRef} 
